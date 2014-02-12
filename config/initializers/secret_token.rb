7f9212008c56de7da5cc474baa525791bb4d375e
rangelfelipe <fe.rra@live.com>
2014-02-12 20:47:49 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hospital::Application.config.secret_key_base = 'eecccde7849b6cd0abf3b2c006d3d3ad87bcac04b4760f8e190590f7a3e4dc2053ba08a6514b3158bdfe0008c7e3d2af75d5a6432b400c8ed9955a3d503af00d'
